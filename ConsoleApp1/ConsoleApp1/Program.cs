bae2c740a6e1a7dd2f217853d5e91742fcdbe049
grin82 <grin82@gmail.com>
2017-07-07 12:15:11 +0300

    test 6

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -5,6 +5,7 @@ namespace ConsoleApp1
     class Program
     {
         //comment
+        //comment 2
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
